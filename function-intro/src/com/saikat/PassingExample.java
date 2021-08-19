cd360c1716410fd7ecb17c4c6764fb8ddb87041b
codernoob1 <79470721+codernoob1@users.noreply.github.com>
2021-08-19 15:36:05 +0530

    functions

---
+++ function-intro/src/com/saikat/PassingExample.java
@@ -0,0 +1,12 @@
+package com.saikat;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String chacha = "Iron Man";
+        greet(chacha);
+    }
+
+    static void greet(String naam) {
+        System.out.println(naam);
+    }
+}
